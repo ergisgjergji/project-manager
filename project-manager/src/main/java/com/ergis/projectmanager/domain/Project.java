c2d83f7475f4e79e898d096814def87709df448c
Ergis Gjergji <ergisgjergji@gmail.com>
2020-05-25 10:27:53 +0200

    Project - Object and Repository

--- project-manager/src/main/java/com/ergis/projectmanager/domain/Project.java
+++ project-manager/src/main/java/com/ergis/projectmanager/domain/Project.java
@@ -1,4 +1,95 @@
 package com.ergis.projectmanager.domain;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String code;
+    private String description;
+
+    private Date start_date;
+    private Date end_date;
+    private Date created_date;
+    private Date updated_date;
+
+    public Project(){
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.created_date = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updated_date = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+
+    public Date getCreated_date() {
+        return created_date;
+    }
+
+    public void setCreated_date(Date created_date) {
+        this.created_date = created_date;
+    }
+
+    public Date getUpdated_date() {
+        return updated_date;
+    }
+
+    public void setUpdated_date(Date updated_date) {
+        this.updated_date = updated_date;
+    }
 }
---
+++ project-manager/src/main/java/com/ergis/projectmanager/repositories/IProjectRepository.java
@@ -0,0 +1,11 @@
+package com.ergis.projectmanager.repositories;
+
+import com.ergis.projectmanager.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
