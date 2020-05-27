8c5c70146d7cd064df40b2be7b36e3425354637f
Ergis Gjergji <ergisgjergji@gmail.com>
2020-05-27 05:37:06 +0200

    Field validations

--- project-manager/src/main/java/com/ergis/projectmanager/domain/Project.java
+++ project-manager/src/main/java/com/ergis/projectmanager/domain/Project.java
@@ -1,6 +1,10 @@
 package com.ergis.projectmanager.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -8,13 +12,22 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project name is required")
     private String name;
+    @NotBlank(message = "Project Code is required")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String code;
+    @NotBlank(message = "Project description is required")
     private String description;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_date;
 
     public Project(){
