db597f5b4218644df4fc441c2b8c201f37214681
Ergis Gjergji <ergisgjergji@gmail.com>
2020-05-27 22:10:00 +0200

    Installed bootstrap package

--- project-manager-react-client/src/components/Dashboard.js
+++ project-manager-react-client/src/components/Dashboard.js
@@ -5,7 +5,7 @@ class Dashboard extends Component {
     render() {
         return (
             <div>
-                <h1>Welcome to the dashboard.</h1>
+                <h1 className="alert alert-warning">Welcome to the dashboard.</h1>
                 <ProjectItem/>
                 <ProjectItem/>
             </div>
