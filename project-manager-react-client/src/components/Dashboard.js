ccad833d82fad76958d0aa74da9e4a30f0ac3ed6
Ergis Gjergji <ergisgjergji@gmail.com>
2020-05-27 19:00:22 +0200

    First react component

---
+++ project-manager-react-client/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to the dashboard.</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Dashboard;
