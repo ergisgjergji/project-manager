688d7ef86d538e4faddd35c1123060668677b738
Ergis Gjergji <ergisgjergji@gmail.com>
2020-05-27 23:09:23 +0200

    Styled Navbar, Dashboard, ProjectItem'
    '

--- project-manager-react-client/src/components/Dashboard.js
+++ project-manager-react-client/src/components/Dashboard.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
-import ProjectItem from './Project/ProjectItem';
+import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
 
 class Dashboard extends Component {
-    render() {
-        return (
-            <div>
-                <h1 className="alert alert-warning">Welcome to the dashboard.</h1>
-                <ProjectItem/>
-                <ProjectItem/>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="projects">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-12">
+							<h1 className="display-4 text-center">Projects</h1>
+							<br />
+							<a href="ProjectForm.html" className="btn btn-lg btn-info">
+								Create a Project
+							</a>
+							<br />
+							<hr />
+							<ProjectItem />
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Dashboard;
