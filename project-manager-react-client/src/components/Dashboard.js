ddf7007433b5c9a5616a48afaf8ff1061ba04027
Ergis Gjergji <ergisgjergji@gmail.com>
2020-05-29 05:43:53 +0200

    Get projects - finalising

--- project-manager-react-client/src/components/Dashboard.js
+++ project-manager-react-client/src/components/Dashboard.js
@@ -13,6 +13,8 @@ class Dashboard extends Component {
 	}
 
 	render() {
+		const { projects } = this.props.projectReducer;
+
 		return (
 			<div className="projects">
 				<div className="container">
@@ -23,7 +25,11 @@ class Dashboard extends Component {
 							<CreateProjectButton/>
 							<br />
 							<hr />
-							<ProjectItem />
+							{ 
+								projects.map(project => (
+									<ProjectItem key={project.id} project={project}/>
+								)
+							)}
 						</div>
 					</div>
 				</div>
