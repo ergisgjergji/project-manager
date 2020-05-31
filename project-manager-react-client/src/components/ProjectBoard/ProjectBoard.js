f9c468ed03306422d80892b4cc892281c53e5b42
Ergis Gjergji <ergisgjergji@gmail.com>
2020-05-31 05:08:18 +0200

    Load ProjectTasks to state (no validation yet)

--- project-manager-react-client/src/components/ProjectBoard/ProjectBoard.js
+++ project-manager-react-client/src/components/ProjectBoard/ProjectBoard.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getBacklog } from '../../redux/actions/backlogActions';
+
 import Backlog from './Backlog';
 
 class ProjectBoard extends Component {
 
-    render() {
-        
-        const { code } = this.props.match.params;
+    // constructor to handle errors
 
+    componentDidMount() {
+        const { code } = this.props.match.params;
+        this.props.getBacklog(code);
+    }
+
+    render() {
+        const { code } = this.props.match.params;
         return (
             <div className="container">
 
@@ -23,4 +32,13 @@ class ProjectBoard extends Component {
     }
 }
 
-export default ProjectBoard;
+ProjectBoard.propTypes = {
+    backlogStore: PropTypes.object.isRequired,
+    getBacklog: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    backlogStore: state.backlogStore
+});
+
+export default connect(mapStateToProps, { getBacklog })(ProjectBoard);
--- project-manager-react-client/src/redux/actions/backlogActions.js
+++ project-manager-react-client/src/redux/actions/backlogActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, GET_BACKLOG } from './types';
 import { clearErrors } from './errorActions';
 
 export const createProjectTask = (code, project_task, history) => dispatch => {
@@ -13,4 +13,14 @@ export const createProjectTask = (code, project_task, history) => dispatch => {
             type: GET_ERRORS,
             payload: err.response.data
         }));
+}
+
+export const getBacklog = (code) => dispatch => {
+
+    axios.get(`/api/backlog/${code}`)
+        .then(res => dispatch({
+            type: GET_BACKLOG,
+            payload: res.data
+        }))
+        .catch(err => console.log(err));
 }
