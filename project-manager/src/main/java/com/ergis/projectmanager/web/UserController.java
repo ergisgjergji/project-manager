5dead108de41cf6b568bf7b451b387c7ea62d1e6
Ergis Gjergji <ergisgjergji@gmail.com>
2020-06-05 10:14:42 +0200

    Added CrossOrigin to UserController

--- project-manager/src/main/java/com/ergis/projectmanager/web/UserController.java
+++ project-manager/src/main/java/com/ergis/projectmanager/web/UserController.java
@@ -14,10 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -25,6 +22,7 @@ import static com.ergis.projectmanager.security.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin
 public class UserController {
 
     @Autowired
